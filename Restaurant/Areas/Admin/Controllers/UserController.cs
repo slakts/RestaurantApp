28411e0b570304ac93c86d82e36a09223acad7ab
slakts <126762743+slakts@users.noreply.github.com>
2025-02-24 14:50:39 +0300

    KULLANICILAR

---
+++ Restaurant/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Models;
+
+namespace Restaurant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly VeriTabaniContext _context;
+        public UserController(VeriTabaniContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _context.ApplicationUsers.ToList();
+            var role = _context.Roles.ToList();
+            var userRol = _context.UserRoles.ToList();
+            foreach (var item in users)
+            {
+                var roleId = userRol.FirstOrDefault(i => i.UserId == item.Id).RoleId;
+                item.Role = role.FirstOrDefault(u => u.Id == roleId).Name;
+            }
+            return View(users);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.ApplicationUsers
+                .FirstOrDefaultAsync(m => m.Id == id.ToString());
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Admin/Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var user = await _context.ApplicationUsers.FindAsync(id);
+            _context.ApplicationUsers.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
